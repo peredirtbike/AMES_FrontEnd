2d038fb26ea760f4e364136b10a852a42fe361c9
Peredirtbike <peredirtbike@gmail.com>
2023-07-25 23:42:49 +0200

    front finished

--- src/views/ClientForm.vue
+++ src/views/ClientForm.vue
@@ -1,34 +1,36 @@
 <template>
   <div class="flex card justify-content-center align-items-center" style="height: 100vh;">
-    <div class="card p-4 shadow-8 border-round">
+    <div class="card p-5 shadow-8 border-round">
 
-      <div class="flex align-items-center justify-content-between mb-4" >
-        <span class="text-xl text-900 font-bold">{{ isUpdating ? 'Actualizar' : 'Crear' }} Client</span>
-        <Button class="" @click="redirectToClientsPage">Tornar</Button>
+      <div class="flex align-items-center justify-content-between mb-4">
+        <span class="text-xl text-900 font-bold">{{ isUpdating ? 'Actualitzar' : 'Crear' }} Client</span>
+        <Button class="hidden-sm hidden-xs" @click="redirectToClientsPage"> <i class="pi pi-home mr-2"></i>Tornar</Button>
+        <Button class="hidden-md hidden-lg" @click="redirectToClientsPage"> <i class="pi pi-home"></i></Button>
       </div>
-      <form @submit.prevent="saveClient" class="formgrid grid">
-        <div class="field col-12 md:col-6 mb-3">
+
+      <form @submit.prevent="saveClient" class="formgrid grid max-w-30rem">
+        <div class="field col-12 md:col-12 mb-3">
           <label for="name">Nom</label>
           <InputText v-model="client.name" id="name" class="text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary w-full" required />
         </div>
-        <div class="field col-12 md:col-6 mb-3">
+        <div class="field col-12 md:col-12 mb-3">
           <label for="lastName">Cognoms</label>
           <InputText v-model="client.lastName" id="lastName" class="text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary w-full" required />
         </div>
-        <div class="field col-12 md:col-8 mb-3">
-          <label for="address">Adreça</label>
-          <InputText v-model="client.address" id="address" class="text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary w-full" required />
-        </div>
-        <div class="field col-12 md:col-4 mb-3">
-          <label for="phone">Telèfon</label>
-          <InputText v-model="client.phone" id="phone" class="text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary w-full" required />
-        </div>
-        <div class="field col-12 md:col-6 mr-4 mb-3">
+        <div class="field col-12 md:col-7 mb-3">
           <label for="email">Email</label>
           <InputText v-model="client.email" id="email" class="text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary w-full" required />
         </div>
+        <div class="field col-12 md:col-5 mb-3">
+          <label for="phone">Telèfon</label>
+          <InputText v-model="client.phone" id="phone" class="text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary w-full" required />
+        </div>
+        <div class="field col-12 mb-3">
+          <label for="address">Adreça</label>
+          <InputText v-model="client.address" id="address" class="text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary w-full" required />
+        </div>
 
-        <div class="card flex flex-wrap gap-4 ml-2">
+        <div class="card flex flex-wrap gap-4 ml-2 py-4 mb-3">
           <div class="flex align-items-center">
             <Checkbox v-model="client.preferential" inputId="preferential" name="preferential" :binary="true" />
             <label for="preferential" class="ml-2">És un client preferencial?</label>
@@ -39,13 +41,40 @@
           </div>
         </div>
 
-        <div class="flex field col-12 mt-4">
-          <Button type="submit" :label="isUpdating ? 'Actualitzar' : 'Crear'" />
+        <div class="col-12">
+          <Button type="submit">
+            <i v-if="!isUpdating" class="pi pi-user-plus mr-2"></i>
+            <i v-else class="pi pi-check mr-2"></i>
+            {{ isUpdating ? 'Actualizar' : 'Crear' }}
+          </Button>
         </div>
       </form>
     </div>
   </div>
 </template>
+
+<style>
+@media (max-width: 767px) {
+  .hidden-xs {
+    display: none !important;
+  }
+}
+@media (min-width: 768px) and (max-width: 991px) {
+  .hidden-sm {
+    display: none !important;
+  }
+}
+@media (min-width: 992px) and (max-width: 1199px) {
+  .hidden-md {
+    display: none !important;
+  }
+}
+@media (min-width: 1200px) {
+  .hidden-lg {
+    display: none !important;
+  }
+}
+</style>
 
 <script>
 import { mapActions, mapGetters } from 'vuex';
--- src/views/ClientList.vue
+++ src/views/ClientList.vue
@@ -1,34 +1,37 @@
 
 <template>
   <div>
-    <DataTable :value="filteredClients(searchTerm)" tableStyle="min-width: 50rem">
+    <DataTable  :value="filteredClients(searchTerm)" sortMode="multiple" tableStyle="min-width: 50rem">
       <template #header>
-          <div class="flex align-items-center justify-content-between gap-2">
+          <div class="flex flex-wrap align-items-center justify-content-between gap-1 grid">
               <span class="text-xl text-900 font-bold">Clients</span>
-              <Button class="right-at" @click="redirectToNewClientForm">Crea un client</Button>
+              <span class="p-input-icon-left">
+                <i class="pi pi-search" />
+                <InputText class="max-w-30rem" size="small" v-model="searchTerm" placeholder="Cerca un client" />
+              </span>
+                <Button class="hidden-xs hidden-sm" @click="redirectToNewClientForm"><i class="pi pi-user-plus mr-2"></i> Crear Client</Button>
+                <Button class="hidden-md hidden-lg" @click="redirectToNewClientForm"><i class="pi pi-user-plus"></i></Button>
           </div>
 
-          <div class="flex align-items-center gap-2">
-              <InputText size="small" v-model="searchTerm" placeholder="Cerca un client" />
-          </div>
       </template>
-      <Column field="name" header="Nom"></Column>
-      <Column field="lastName" header="Cognoms"></Column>
-      <Column field="phone" header="Telèfon"></Column>
-      <Column field="email" header="Email"></Column>
-      <Column field="address" header="Adreça"></Column>
-      <Column field="hasDiscount" header="Descompte">
+      <Column sortable field="name" header="Nom"></Column>
+      <Column sortable field="lastName" header="Cognoms"></Column>
+      <Column sortable field="phone" header="Telèfon"></Column>
+      <Column sortable field="email" header="Email"></Column>
+      <Column sortable field="address" header="Adreça"></Column>
+      <Column sortable field="hasDiscount" header="Descompte">
         <template #body="rowData">
           <i :class="{'pi pi-check': rowData.data.preferential, 'pi pi-times': !rowData.data.preferential}"></i>
         </template>
       </Column>
-      <Column field="preferential" header="Preferencial">
+      <Column sortable field="preferential" header="Preferencial">
         <template #body="rowData">
           <i :class="{'pi pi-check': rowData.data.preferential, 'pi pi-times': !rowData.data.preferential}"></i>
         </template></Column>
       <Column header="Accions">
         <template #body="rowData">
-          <Button @click="editClient(rowData.data.id)">Editar</Button>
+          <Button class="hidden-xs hidden-sm" @click="editClient(rowData.data.id)"> <i class="pi pi-user-edit mr-2"></i>Editar</Button>
+          <Button class="hidden-md hidden-lg" @click="editClient(rowData.data.id)"> <i class="pi pi-user-edit"></i></Button>
         </template>
       </Column>
 
@@ -37,6 +40,28 @@
 </div>
 </template>
 
+<style>
+@media (max-width: 767px) {
+  .hidden-xs {
+    display: none !important;
+  }
+}
+@media (min-width: 768px) and (max-width: 991px) {
+  .hidden-sm {
+    display: none !important;
+  }
+}
+@media (min-width: 992px) and (max-width: 1199px) {
+  .hidden-md {
+    display: none !important;
+  }
+}
+@media (min-width: 1200px) {
+  .hidden-lg {
+    display: none !important;
+  }
+}
+</style>
 
 <script>
 import { mapActions, mapGetters, mapState } from 'vuex';
